6d54aaeef7e6968a1641d47839e1ede898229c4a
depich <dmitriysvitok@gmail.com>
2025-04-20 08:55:49 +0300

    Emai

--- app/bookings/shemas.py
+++ app/bookings/shemas.py
@@ -10,6 +10,8 @@ class CreateBookingsSchema(BaseModel):
     room_id: int
     user_id: int
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class UpdateBookingsSchema(BaseModel):
     date_from: date
